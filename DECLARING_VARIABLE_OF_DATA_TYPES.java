e911b14596edb87e83153a0863d4bf1a94824a46
keysersoose <90134774+keysersoose@users.noreply.github.com>
2021-09-05 19:04:24 +0530

    DECLARING_VARIABLE_OF_DATA_TYPES.java

---
+++ DECLARING_VARIABLE_OF_DATA_TYPES.java
@@ -0,0 +1,12 @@
+
+public class DECLARING_VARIABLE_OF_DATA_TYPES {
+
+	public static void main(String[] args) {
+		int number= 7;
+		float floatnumber = 3.14f;
+		char character = 'A';
+		boolean booleanval = true;
+		String name = "Gokul";
+		}
+
+}
