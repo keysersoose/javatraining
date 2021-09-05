3bfb6165bfc591a6cf423fc98a3e4e5c421b7c71
keysersoose <90134774+keysersoose@users.noreply.github.com>
2021-09-05 19:35:37 +0530

    Declaring_variable_of_data_type.java

---
+++ Declaring_variable_of_data_type.java
@@ -0,0 +1,13 @@
+
+public class Declaring_variable_of_data_type {
+
+	public static void main(String[] args) {
+		int number= 19;
+		float floatnumber = 15f;
+		char character = 'B';
+		boolean booleanval = true;
+		String name = "Suraj";
+
+	}
+
+}
